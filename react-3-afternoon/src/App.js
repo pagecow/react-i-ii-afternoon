a21d48de2e7d6aa904ae3207987f1cdcee0f721c
pagecow <pagecow@gmail.com>
2019-09-25 15:45:07 -0600

    more finished

--- react-3-afternoon/src/App.js
+++ react-3-afternoon/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Name from './Components/Name';
+import FromJobEmployer from './Components/FromJobEmployer';
+import FavoriteMovies from './Components/FavoriteMovies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <body>
+        {/* HEADER */}
+        <header>
+          <div className='logo'>Home</div>
+        </header>
+
+        {/* BODY */}
+        <div>
+          <div className='white-box'>
+            <div className='name'>
+              <Name />
+            </div> 
+            <div className='from-job-employer'>
+              <FromJobEmployer />
+            </div>
+            <div className='favorite-movies'>
+              <FavoriteMovies />
+            </div> 
+          </div>
+        </div>
+
+      </body>
     </div>
   );
 }
