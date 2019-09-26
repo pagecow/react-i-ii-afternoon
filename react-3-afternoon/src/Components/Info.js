900d88ecb0f62a22c28aae4bbfe384a0b186c4fe
pagecow <pagecow@gmail.com>
2019-09-26 13:42:12 -0600

    finished. Can slide back and forth between slide 1 and 2.

--- react-3-afternoon/src/Components/Info.js
+++ react-3-afternoon/src/Components/Info.js
@@ -12,14 +12,14 @@ class Info extends Component {
             jobTitle: 'Physical Therapy Assistant',
             employer: 'Twinder',
             favoriteMovies: [
-            "That Night in Varennes (Nuit de Varennes, La), Spy(ies) (Espion(s)), Klip (Clip)"],
+            "1. That Night in Varennes (Nuit de Varennes, La), 2. Spy(ies) (Espion(s)), 3. Klip (Clip)"],
 
             idArray: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
             nameArray:["Waylin Lumsdon", 'Hastie Beardsdale', ],
             fromArray: ["Likiep, Marshall Islands", 'Dzvinogrud, Ukraine', ],
             jobTitleArray: ["Physical Therapy Assistant", 'Data Coordiator', ],
             employerArray: ["Twinder", 'Vinte', ],
-            favoriteMoviesArray: [["That Night in Varennes (Nuit de Varennes, La), Spy(ies) (Espion(s)), Klip (Clip)"], ["Camp, Her Highness and the Bellboy, Mona and the Time of Burning Love (Mona ja palavan rakkauden aika))"], ]          
+            favoriteMoviesArray: [["1. That Night in Varennes (Nuit de Varennes, La), 2. Spy(ies) (Espion(s)), 3. Klip (Clip)"], ["1. Camp, 2. Her Highness and the Bellboy, 3. Mona and the Time of Burning Love (Mona ja palavan rakkauden aika))"], ]          
     }
 }
 
@@ -39,7 +39,7 @@ class Info extends Component {
                 <h3>Favorite Movies:</h3>
                 
                 <h3 favoriteMovies={this.state.favoriteMovies}>
-                    <li>{this.state.favoriteMovies}</li>
+                    <lu>{this.state.favoriteMovies}</lu>
                     {/* <li>{this.state.favoriteMovies.map(1)}</li>
                     <li>{this.state.favoriteMovies.map(1)}</li> */}
                 </h3>
